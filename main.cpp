97c04b63a24fa1690ed406eff6a95add33ab4d15
Esvin González <usac.ing.esvin@gmail.com>
2016-12-27 01:23:31 -0600

    Conflictos con main.cpp resuletos...

--- main.cpp
+++ main.cpp
@@ -7,10 +7,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Principal w;
     w.show();
-<<<<<<< HEAD
-=======
-
-
->>>>>>> cf64e9f08672a095e97e4802ed147df62fe5d015
     return a.exec();
 }
